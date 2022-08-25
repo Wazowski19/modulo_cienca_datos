60874ef475a8a209b0450d18c28d67819c0602ab
Miguel Ángel García Martínez <ma.garmar98@gmail.com>
2022-08-24 21:25:29 -0500

    feat: sets

---
+++ basics/sets.py
@@ -0,0 +1,12 @@
+#Sets
+
+#They are unindexed
+#They are unordered
+#They NOT allow duplicated values
+
+exampleSet = {"blue", "yellow", "red", "pink"}
+exampleSet2 = set({"blue", "yellow", "red", "pink", "purple"})
+
+print(type(exampleSet))
+print(exampleSet)
+print(exampleSet2)
